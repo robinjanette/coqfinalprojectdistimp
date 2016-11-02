d1fa922a4e54912c72b38deb28a3bcb572afca9d
Alex Snyder <alexetsnyder@gmail.com>
2016-11-02 17:20:49 -0400

    Start copied from Smallstep.v

--- dist_imp.v
+++ dist_imp.v
@@ -5,3 +5,27 @@ Require Import Coq.Arith.EqNat.
 Require Import Maps.
 Require Import Imp.
 
+Inductive com : Type :=
+  | CSkip : com
+  | CAss : id -> aexp -> com
+  | CSeq : com -> com -> com
+  | CIf : bexp -> com -> com -> com
+  | CWhile : bexp -> com -> com
+  (* Maybe, Probably not. *)
+  | CDist : com -> com -> com.
+
+Notation "'SKIP'" :=
+  CSkip.
+Notation "x '::=' a" :=
+  (CAss x a) (at level 60).
+Notation "c1 ;; c2" :=
+  (CSeq c1 c2) (at level 80, right associativity).
+Notation "'WHILE' b 'DO' c 'END'" :=
+  (CWhile b c) (at level 80, right associativity).
+Notation "'IFB' b 'THEN' c1 'ELSE' c2 'FI'" :=
+  (CIf b c1 c2) (at level 80, right associativity).
+Notation "'DIST' c1 'WITH' c2 'END'" :=
+  (CDist c1 c2) (at level 80, right associativity).
+
+
+
