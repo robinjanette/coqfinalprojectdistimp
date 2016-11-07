ea5b29c3c8bafd9c3e8bbff3073b1d816fd1e777
Alex Snyder <alexetsnyder@gmail.com>
2016-11-07 14:07:19 -0500

    Started with Send and Recive.

--- dist_imp.v
+++ dist_imp.v
@@ -11,8 +11,9 @@ Inductive com : Type :=
   | CSeq : com -> com -> com
   | CIf : bexp -> com -> com -> com
   | CWhile : bexp -> com -> com
-  (* Maybe, Probably not. *)
-  | CDist : com -> com -> com.
+  (* Distributed Commands*)
+  | CSend : id -> aexp -> com
+  | CRecieve: com.
 
 Notation "'SKIP'" :=
   CSkip.
@@ -24,8 +25,35 @@ Notation "'WHILE' b 'DO' c 'END'" :=
   (CWhile b c) (at level 80, right associativity).
 Notation "'IFB' b 'THEN' c1 'ELSE' c2 'FI'" :=
   (CIf b c1 c2) (at level 80, right associativity).
-Notation "'DIST' c1 'WITH' c2 'END'" :=
-  (CDist c1 c2) (at level 80, right associativity).
+
+
+Reserved Notation " t '/' st '==>' t' '/' st' "
+                  (at level 40, st at level 39, t' at level 39).
+
+Inductive cstep : (com * state) -> (com * state) -> Prop :=
+  | CS_AssStep : forall st i a a',
+      a / st ==>a a' ->
+      (i ::= a) / st ==> (i ::= a') / st
+  | CS_Ass : forall st i n,
+      (i ::= (ANum n)) / st ==> SKIP / (t_update st i n)
+  | CS_SeqStep : forall st c1 c1' st' c2,
+      c1 / st ==> c1' / st' ->
+      (c1 ;; c2) / st ==> (c1' ;; c2) / st'
+  | CS_SeqFinish : forall st c2,
+      (SKIP ;; c2) / st ==> c2 / st
+  | CS_IfTrue : forall st c1 c2,
+      IFB BTrue THEN c1 ELSE c2 FI / st ==> c1 / st
+  | CS_IfFalse : forall st c1 c2,
+      IFB BFalse THEN c1 ELSE c2 FI / st ==> c2 / st
+  | CS_IfStep : forall st b b' c1 c2,
+      b / st ==>b b' ->
+          IFB b THEN c1 ELSE c2 FI / st 
+      ==> (IFB b' THEN c1 ELSE c2 FI) / st
+  | CS_While : forall st b c1,
+          (WHILE b DO c1 END) / st
+      ==> (IFB b THEN (c1;; (WHILE b DO c1 END)) ELSE SKIP FI) / st
+
+  where " t '/' st '==>' t' '/' st' " := (cstep (t,st) (t',st')).
 
 
 
